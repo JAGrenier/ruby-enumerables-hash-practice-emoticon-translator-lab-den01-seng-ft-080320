a43c4101dce3f25dd0ae0e02ef344b7926292f1a
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-28 20:37:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,44 @@
-# require modules here
 require 'yaml'
-#require 'pry'
 
+# Write a method that loads the emoticons.yml file.
 def load_library(path)
-  emoticon = {"get_meaning" => {},"get_emoticon" =>{}}
+  #set a hash with get_meaning, get_emoticon as keys, empty hash for value
+  emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
+  #load the YAML file. It has meaning(angel, angry,..) as key and the symbol as value(describe)
   YAML.load_file(path).each do |meaning, describe|
-  eng,jan = describe
-  emoticons["get_meaning"][jan] = meaning
-  emoticons["get_emoticon"][eng]= jan
+     #set eng, jan from describe, so eng would be the first value, jan the second
+     eng, jan = describe
+     emoticons["get_meaning"][jan] = meaning
+     emoticons["get_emoticon"][eng] = jan
   end
   emoticons
 end
+
+# get japanese meaning:
+# accepts two arguments, the YAML file path and the emoticon
+# calls on #load_library and gives it the argument of the file path
+# returns the Japanese equivalent of an English emoticons
+
+def get_japanese_emoticon(path, emoticon)
+  emoticons = load_library(path) #call load_library
+  result = emoticons["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
+
+#result:
+get_japanese_emoticon(path, "=D")).to eq("(￣ー￣)
+
+# get_english_meaning: returns the English meaning of the Japanese emoticon
+
+def get_english_meaning(path, emoticon)
+  emoticons = load_library(path)
+  result = emoticons["get_meaning"][emoticon] #the same above but change get_meaning
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
