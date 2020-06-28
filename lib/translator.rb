9df77c862b0fe46306fd89e89c3e37d7ebaf6dc6
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-28 20:24:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,9 @@ def load_library(file)
   emoticons.each do |english_word, emoticon_set|
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
     emoticon_hash["get_meaning"][emoticon_set.last] = english_word
-end
+  end
+  emoticon_hash
+end 
 
 
 def get_japanese_emoticon
