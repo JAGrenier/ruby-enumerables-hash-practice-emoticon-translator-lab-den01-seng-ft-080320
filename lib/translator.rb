525a80df65166369f16661695eaca7b1a1aa86d3
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-28 20:35:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,6 @@
 # require modules here
 require 'yaml'
-require 'pry'
+#require 'pry'
 
 def load_library(path)
   emoticon = {"get_meaning" => {},"get_emoticon" =>{}}
@@ -12,21 +12,3 @@ def load_library(path)
   emoticons
 end
 
-def get_japanese_emoticon(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found"
-  end
-  result
-end
-
-def get_english_meaning(path, emoticon)
-  emoticon_hash = load_library(path)
-  result = emoticon_hash["get_meaning"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found"
-  end
-  result
-
-end
