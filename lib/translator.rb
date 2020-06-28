cae5194e9a2dc789accab4ce90b49f329226b962
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-28 20:27:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def load_library(path)
     emoticon_hash["get_meaning"][emoticon_set.last] = english_word
   end
   emoticon_hash
-  binding.pry
+
 end
 
 def get_japanese_emoticon(path, emoticon)
@@ -33,4 +33,5 @@ def get_english_meaning(path, emoticon)
     result = "Sorry, that emoticon was not found"
   end
   result
+    binding.pry
 end
